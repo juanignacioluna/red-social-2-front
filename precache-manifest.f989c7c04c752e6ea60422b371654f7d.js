18f4645bc41d036cd50bbd23ebaea63bc65197a8
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-01-18 07:50:15 -0300

    Updates

---
+++ precache-manifest.f989c7c04c752e6ea60422b371654f7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e2afff5a2c32e66d0e9bc22bc8b226",
+    "url": "/red-social-2-front/index.html"
+  },
+  {
+    "revision": "8db0d866d92ab24e48d0",
+    "url": "/red-social-2-front/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "0508619dace2cfc54827",
+    "url": "/red-social-2-front/static/css/main.a52ccd08.chunk.css"
+  },
+  {
+    "revision": "8db0d866d92ab24e48d0",
+    "url": "/red-social-2-front/static/js/2.5abccca3.chunk.js"
+  },
+  {
+    "revision": "0508619dace2cfc54827",
+    "url": "/red-social-2-front/static/js/main.4dffc99a.chunk.js"
+  },
+  {
+    "revision": "13a279d510fb2b3d056d",
+    "url": "/red-social-2-front/static/js/runtime-main.8fbe9756.js"
+  },
+  {
+    "revision": "79847a58081573dc6ea0e64868f51e20",
+    "url": "/red-social-2-front/static/media/ig6.79847a58.png"
+  }
+]);
